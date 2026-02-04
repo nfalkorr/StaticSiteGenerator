e33fca17f152b70baf5c567feca942fa67649c26
user <nfasan@nfalkorr.org>
2026-02-03 21:50:01 -0800

    staging initial changes to work on delimiters

---
+++ src/delimiters.py
@@ -0,0 +1,10 @@
+import htmlnode
+import textnode
+
+def split_nodes_delimiter(old_nodes,delimiter,text_type):
+	#if old node not in texttypes
+		#add to new list no modifications
+	#else if raise exception invalid markdown syntax
+	#.split() by delimiters
+	#.extend() each element to the list
+	pass
---
+++ src/test_delimiters.py
@@ -0,0 +1,3 @@
+#import delimiters
+
+#tests go here
